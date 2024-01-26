e3ffef90f24fe201d9af4cd8c99ccbc26eb969d3
Y2Kmeltdown <damien.rice123@gmail.com>
2024-01-26 13:40:16 +1100

    Updated results

--- Assembly/NeuroRISCAnalysis.py
+++ Assembly/NeuroRISCAnalysis.py
@@ -4,17 +4,50 @@ import numpy as np
 import time
 import os
 
-v = -65
-u = 0
-I = 10
+neuronType="CH"
 
-a = 0.1
-b = 0.2
-c = -65
-d = 2
+if neuronType == "IB":
+    v = -55
+    u = 0
+    I = 10
+
+    a = 0.02
+    b = 0.2
+    c = -55
+    d = 4
+elif neuronType == "RS":
+    v = -65
+    u = 0
+    I = 10
+
+    a = 0.02
+    b = 0.2
+    c = -65
+    d = 8
+
+elif neuronType == "CH":
+    v = -50
+    u = 0
+    I = 10
+
+    a = 0.02
+    b = 0.2
+    c = -50
+    d = 2
+
+elif neuronType == "FS":
+    v = -65
+    u = 0
+    I = 10
+
+    a = 0.1
+    b = 0.2
+    c = -65
+    d = 2
 
 
-filename = f"C:\\Users\\damie\\Documents\\Coding Projects\\Verilog\\NeuroRISC\\Results Data\\izhikevichFS{I}Stimulus.csv"
+
+filename = f"C:\\Users\\damie\\Documents\\Coding Projects\\Verilog\\NeuroRISC\\Results Data\\izhikevich{neuronType}{I}Stimulus.csv"
 
 file = filename
 print(file)
@@ -47,7 +80,7 @@ vData = [v]
 vTime = [0]
 vTimeInit = time.perf_counter_ns()
 t = time.perf_counter_ns()
-for n in range(len(membraneDataFiltered.values)):
+for n in range(len(membraneDataFiltered.values)-1):
     tNew = time.perf_counter_ns() - vTimeInit
     dt = tNew - t
     dtsecs = dt*10**-9
@@ -66,14 +99,32 @@ for n in range(len(membraneDataFiltered.values)):
         u+=d
     t = tNew
 
+error = []
+for vS, vF in zip(vData,membraneDataFiltered.values):
+    error.append(abs(((vF-vS)/vS)*100))
+
+print(error)
+
 vTimeFPGA = [tValue-timeData.values[0] for tValue in timeData.values]
 
-p = plt.plot(membraneDataFiltered.values, color='r', label='FPGA')
-p = plt.plot(vData, color='g', label='Software')
-plt.title("Spike Train with current input")
+fig, (ax1, ax2) = plt.subplots(2,1,sharey=True)
+ax1.plot(membraneDataFiltered.values, color='r', label='FPGA')
+ax1.set_title("FPGA Neuron")
+#ax1.ylabel("Membrane Potential(mV)")
+ax2.plot(vData, color='g', label='Software')
+ax2.set_title("Software Neuron")
+#ax2.ylabel("Membrane Potential(mV)")
+
+#p = plt.plot(membraneDataFiltered.values, color='r', label='FPGA')
+#p = plt.plot(error, color='b', label='Software')
+#plt.title("Relative Error of integer based calculation.")
 plt.xlabel("TimeStep")
 plt.ylabel("Membrane Potential (mV)")
-plt.legend()
+#plt.legend()
+#plt.show()
+
+#print(len(vData))
+#print(len(membraneDataFiltered.values))
+#plt.savefig(f"C:\\Users\\damie\\Documents\\Coding Projects\\Verilog\\NeuroRISC\\results\\FPGAvsSoftwareFS{I}StimulusUnitless")
 plt.show()
-plt.savefig(f"C:\\Users\\damie\\Documents\\Coding Projects\\Verilog\\NeuroRISC\\results\\FPGAvsSoftwareFS{I}StimulusUnitless")
 
